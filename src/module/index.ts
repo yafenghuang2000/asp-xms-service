0778eba0882d485aaeaf4ecace09a47330761da0
yafenghuang2000 <yafenghuang2000@gmail.com>
2025-02-15 23:04:02 +0800

    feat: 添加日志拦截器和模块配置，重构模块结构以支持全局配置

--- src/module/index.ts
+++ src/module/index.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { LoggingInterceptor } from '@/utils/logging.interceptor';
-// import { ErrorHandlerService } from '@/utils/response-transformer.interceptor';
-import { JwtGlobalModule } from '@/utils/jwt.global.module';
-import { TypeOrmConfigModule } from '@/utils/typeorm-config.module';
-import { GlobalEntitiesModule } from '@/utils/global-entities.module';
+import { LoggingInterceptor } from '@/utils/log-config/logging.interceptor';
+import { JwtGlobalModule } from '@/utils/module-config/jwt.global.module';
+import { TypeOrmConfigModule } from '@/utils/module-config/typeorm-config.module';
+import { GlobalEntitiesModule } from '@/utils/module-config/global-entities.module';
 import { UserModule } from './user.module';
 
 @Module({
@@ -15,10 +14,6 @@ import { UserModule } from './user.module';
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
     },
-    // {
-    //   provide: APP_INTERCEPTOR,
-    //   useClass: ErrorHandlerService,
-    // },
   ],
 })
 export class AppModule {}
